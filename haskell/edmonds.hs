4c81ebefa3bd99ba03b3cdb08e53da6811db7448
meisterluk <admin@lukas-prokop.at>
2016-03-22 23:51:28 +0100

    Refactor and finalize source code.

--- haskell/edmonds.hs
+++ haskell/edmonds.hs
@@ -1,9 +1,7 @@
-import Control.Monad
 import Data.List (minimumBy, partition, isPrefixOf)
 import Data.Ord (comparing)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import qualified Data.Map as Map
 import System.Exit
 import System.Environment
 
@@ -26,35 +24,41 @@ type Path = [Edge]
 
 -- [PARSING & REPRESENTATION]
 
+-- Given a string representation of an Edge, parse this line
 readEdgeLine :: String -> Edge
-readEdgeLine s = case words s of
+readEdgeLine str = case words str of
   [s,d,w] -> (Edge (read s) (read d) (read w))
   _       -> error "Invalid edge line"
 
+-- Given a string representation of (#vertices, #edges, root), parse this line
 readHeaderLine :: String -> (Integer, Integer, Integer)
 readHeaderLine l = case words l of
   [a,b,c] -> ((read a), (read b), (read c))
   _ -> error "Invalid header line"
 
+-- helper function: dispatch line parsing
 fromString' :: [String] -> Graph
 fromString' [] = (Graph [] [] 0)
 fromString' (l:ls) =
   case readHeaderLine l of
-    (nrV, _, root) -> (Graph [1,2..nrV] edges root)
+    (nrV, _, r) -> (Graph [1,2..nrV] es r)
   where
-    edges = map (readEdgeLine) ls
+    es = map (readEdgeLine) ls
 
+-- Given string representation of a graph, parse lines to retrieve graph
 fromString :: String -> Graph
-fromString src = fromString' (filter (\ l -> noHash l && noBLine l) (lines src))
+fromString str = fromString' (filter (\ l -> noHash l && noBLine l) (lines str))
   where
     noHash = (not . (isPrefixOf "#"))
     noBLine = (not . (isPrefixOf "b "))
 
+-- Given a set of Edges, represent edges as string
 showEdges :: [Edge] -> String -> String
-showEdges _ []          = ""
+showEdges [] _          = ""
 showEdges (e:es) prefix = prefix ++ (show $ src e) ++ " " ++ (show $ dst e) ++
                           " " ++ (show $ weight e) ++ "\n" ++ (showEdges es prefix)
 
+-- Given a graph, represent as string
 toString :: Graph -> String
 toString (Graph vs es r) =
     (show $ maxVertex vs) ++ " " ++
@@ -69,10 +73,13 @@ toString (Graph vs es r) =
 
 -- [AUXILIARY FUNCTIONS]
 
+-- Given a set of edges, determine the smallest weight occuring in this set
 minWeightEdge :: [Edge] -> Edge
 minWeightEdge [] = error "Missing an edge"
 minWeightEdge es = minimumBy (comparing weight) es
 
+-- Given a path, traverse this path backwards until given vertex is found
+-- Returns a subset of the provided path
 backTraversePath :: (Path, Vertex) -> Path
 backTraversePath ([], _) = []
 backTraversePath ((e:p), v) =
@@ -80,6 +87,8 @@ backTraversePath ((e:p), v) =
   then (e:p)
   else (e:(backTraversePath (p, v)))
 
+-- Given a path, traverse it until we hit some vertex already visited
+-- Returns the path traversed and the vertex hit again
 traversePath :: Path -> Path -> Set Vertex -> (Path, Vertex)
 traversePath [] _ _ = ([], -1)
 traversePath (e:p) path visited =
@@ -87,11 +96,14 @@ traversePath (e:p) path visited =
   then (e:path, src e)
   else traversePath p (e:path) (Set.insert (src e) visited)
 
+-- Given a path, retrieve some cycle traversed inside
 getCycle :: Path -> Path
 getCycle [] = []
 getCycle p@(e:_) =
   backTraversePath $ traversePath p [] (Set.fromList [dst e])
 
+-- Given a set of edges, a path traversed so far and an initial vertex
+-- Return set of paths leading to dead ends or cycles (subsets of edges)
 dfs :: [Edge] -> Path -> Vertex -> [Path]
 dfs es base r =
     if null newEdges
@@ -110,6 +122,7 @@ dfs es base r =
 
 -- [EDMONDS HELPERS]
 
+-- Given arbitrary edges (s, d, w), return edges with unique (s, d) and w minimized
 removeMultiEdges :: [Edge] -> [Edge]
 removeMultiEdges []           = []
 removeMultiEdges gedges@(e:_) =
@@ -118,6 +131,8 @@ removeMultiEdges gedges@(e:_) =
     (alike, different) = partition (\ e' -> (src e, dst e) == (src e', dst e')) gedges
     minEdge            = minWeightEdge alike
 
+-- Given a set of edges (s, d, w), retrieve a subset
+-- such that d is unique and w minimized
 cheapestEdges :: [Edge] -> [Edge]
 cheapestEdges []     = []
 cheapestEdges gedges@(e:_) =
@@ -129,6 +144,7 @@ cheapestEdges gedges@(e:_) =
 
 -- [EDMONDS BRANCHING ALGORITHM]
 
+-- map edge to new problem according to EBA
 edmondsEdge :: Edge -> [Edge] -> Set Vertex -> Vertex -> Edge
 edmondsEdge edge@(Edge u v w) es cycleVertices contractVertex
     | not uInC && vInC     = (Edge u contractVertex (w - cheapW))
@@ -139,12 +155,14 @@ edmondsEdge edge@(Edge u v w) es cycleVertices contractVertex
     uInC = Set.member u cycleVertices
     vInC = Set.member v cycleVertices
 
+-- Given a graph and a cycle, retrieve a problem with a contracted vertex.
+-- Call recursion and decontract result. Return this result solving the original problem.
 edmondsCycle :: Graph -> Path -> Graph
-edmondsCycle g' cycle =
+edmondsCycle g' cy =
     (Graph (vertices g') (concat [(filter (/= inCycleUv) cycleEdges), map unmapEdge (edges recurse)]) (root g'))
   where
     newVertex = (maximum $ vertices g') + 1
-    cycleVertices = Set.insert (src $ head cycle) $ Set.fromList $ map dst cycle
+    cycleVertices = Set.insert (src $ head cy) $ Set.fromList $ map dst cy
     inCycle e = (Set.member (src e) cycleVertices && Set.member (dst e) cycleVertices)
     (cycleEdges, otherEdges) = partition inCycle (edges g')
     mapEdge e = edmondsEdge e (edges g') cycleVertices newVertex
@@ -153,8 +171,15 @@ edmondsCycle g' cycle =
     unmapEdge e = fst $ head $ filter ((== e) . snd) associatedEdges
     uvc = head $ filter ((== newVertex) . dst) (edges recurse)
     uv = unmapEdge uvc
-    inCycleUv = head $ filter ((== (dst uv)) . dst) cycle
+    inCycleUv = head $ filter ((== (dst uv)) . dst) cy
 
+-- High-level call for EBA. Given a graph, return a min-weight arborescence
+-- (GHCI example following)
+-- :load *edmonds
+-- let vs = [1,2,3,4,5,6]
+-- let es = [(Edge 1 4 10.0), (Edge 1 2 10.0), (Edge 4 5 2.0), (Edge 1 3 2.0), (Edge 5 2 2.0), (Edge 3 4 4.0), (Edge 4 6 4.0), (Edge 2 3 1.0), (Edge 2 6 8.0)]
+-- let arb = edmonds (Graph vs es 1)
+-- arb
 edmonds :: Graph -> Graph
 edmonds g@(Graph vs es r) =
     if null cycles
